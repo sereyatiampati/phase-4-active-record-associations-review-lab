f8d961d67c74340cdced1f28aa18fd984f6e6516
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-17 15:29:53 +0300

    assignment completed

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string :number_plate
+      t.string :driver_name
 
       t.timestamps
     end
